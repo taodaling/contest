68ede01822909986e0db5ee048ae0904e0486bca
taodaling <daling.tao@ideacome.com>
2019-10-17 09:49:38 +0800

    daily commit

---
+++ src/main/java/contest/TaskE.java
@@ -0,0 +1,10 @@
+package contest;
+
+import template.FastInput;
+import template.FastOutput;
+
+public class TaskE {
+    public void solve(int testNumber, FastInput in, FastOutput out) {
+
+    }
+}
