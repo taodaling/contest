d889bde0ab6fbf5271cf559548f2f9c9b007b4c3
taodaling <taodaling@gmail.com>
2019-11-11 23:39:31 +0800

    cf virtual contest

---
+++ src/test/java/on2019_11/on2019_11_11_Codeforces_Round__586__Div__1___Div__2_/F___Gardener_Alex/Main.java
@@ -0,0 +1,14 @@
+package on2019_11.on2019_11_11_Codeforces_Round__586__Div__1___Div__2_.F___Gardener_Alex;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("src/test/java/on2019_11/on2019_11_11_Codeforces_Round__586__Div__1___Div__2_/F___Gardener_Alex/F - Gardener Alex.json"))
+			Assert.fail();
+	}
+}
