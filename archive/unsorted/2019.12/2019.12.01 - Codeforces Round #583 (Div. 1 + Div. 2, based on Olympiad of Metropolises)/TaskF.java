c96baab45536447f9edb19b73169ac54140a9877
taodaling <taodaling@gmail.com>
2019-12-01 23:28:51 +0800

    solve more

---
+++ archive/unsorted/2019.12/2019.12.01 - Codeforces Round #583 (Div. 1 + Div. 2, based on Olympiad of Metropolises)/TaskF.java
@@ -0,0 +1,52 @@
+package contest;
+
+import template.io.FastInput;
+import template.io.FastOutput;
+import template.math.DigitBase;
+import template.math.DigitUtils;
+import template.rand.Randomized;
+import template.utils.SequenceUtils;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TaskF {
+    public void solve(int testNumber, FastInput in, FastOutput out) {
+        int m = in.readInt();
+        int n = in.readInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.readInt() - 1;
+        }
+        for (int i = 0; i < n; i++) {
+            b[i] = in.readInt() - 1;
+        }
+
+        Randomized.randomizedArray(a, 0, n);
+        Randomized.randomizedArray(b, 0, n);
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        //posSum - canSum is answer
+        long posSum = 0;
+        long canSum = 0;
+        for (int i = 0; i < n; i++) {
+            posSum += b[i];
+            canSum += a[i];
+        }
+
+       
+    }
+
+    int cwDist(int x, int y, int m) {
+        return DigitUtils.mod(y - x, m);
+    }
+
+
+    int ccDist(int x, int y, int m) {
+        return DigitUtils.mod(x - y, m);
+    }
+}
