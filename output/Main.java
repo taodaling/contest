f8711d6d92f260f9fd06a7d5488886b3ec015c93
taodaling <taodaling@gmail.com>
2019-12-06 20:09:55 +0800

    solve more and more, will I be stronger

--- output/Main.java
+++ output/Main.java
@@ -28,362 +28,113 @@ public class Main {
             OutputStream outputStream = System.out;
             FastInput in = new FastInput(inputStream);
             FastOutput out = new FastOutput(outputStream);
-            TaskF2 solver = new TaskF2();
+            TaskF solver = new TaskF();
             solver.solve(1, in, out);
             out.close();
         }
     }
 
-    static class TaskF2 {
-        MultiWayIntStack edges;
-        int[][] dp;
-        int[] color;
-        int[] parent;
-        boolean[] visited;
-        Modular mod = new Modular(998244353);
-        boolean valid = true;
+    static class TaskF {
+        BitOperator bo = new BitOperator();
+        int p;
+        int[] charCnt;
+        int[] dp;
+        int[] cnts;
 
         public void solve(int testNumber, FastInput in, FastOutput out) {
             int n = in.readInt();
-            int k = in.readInt();
-            edges = new MultiWayIntStack(n + 1, n * 2);
-            dp = new int[n + 1][2];
-            color = new int[n + 1];
-            visited = new boolean[n + 1];
-            parent = new int[n + 1];
-
-            for (int i = 1; i <= n; i++) {
-                color[i] = in.readInt();
+            p = in.readInt();
+            int[] s = new int[n];
+            for (int i = 0; i < n; i++) {
+                s[i] = in.readChar() - 'a';
             }
+            int[] mask = new int[p];
 
-            for (int i = 1; i <= n; i++) {
-                int a = in.readInt();
-                int b = in.readInt();
-                edges.addLast(a, b);
-                edges.addLast(b, a);
-            }
-            LcaOnTree lcaOnTree = new LcaOnTree(edges, 1);
-            int[] lca = new int[k + 1];
-            dfsForParent(1, 0);
-            for (int i = 1; i <= n; i++) {
-                if (color[i] == 0) {
-                    continue;
-                }
-                if (lca[color[i]] == 0) {
-                    lca[color[i]] = i;
-                } else {
-                    lca[color[i]] = lcaOnTree.lca(lca[color[i]], i);
+            for (int i = 0; i < p; i++) {
+                for (int j = 0; j < p; j++) {
+                    mask[i] = bo.setBit(mask[i], j, in.readInt() == 0);
                 }
             }
-            for (int i = 1; i <= n; i++) {
-                if (color[i] == 0) {
-                    continue;
+            IntList[] next = new IntList[n];
+            IntList[] prev = new IntList[n];
+
+            prev[0] = new IntList();
+            for (int i = 1; i < n; i++) {
+                prev[i] = new IntList(prev[i - 1].size() + 1);
+                prev[i].addAll(prev[i - 1]);
+                int index = prev[i].indexOf(s[i - 1]);
+                if (index >= 0) {
+                    prev[i].remove(index);
                 }
-                if (i != lca[color[i]]) {
-                    paint(parent[i], lca[color[i]], color[i]);
+                prev[i].add(s[i - 1]);
+            }
+            next[n - 1] = new IntList();
+            for (int i = n - 2; i >= 0; i--) {
+                next[i] = new IntList(next[i + 1].size() + 1);
+                next[i].addAll(next[i + 1]);
+                int index = next[i].indexOf(s[i + 1]);
+                if (index >= 0) {
+                    next[i].remove(index);
+                }
+                next[i].add(s[i + 1]);
+            }
+
+            cnts = new int[1 << p];
+            for (int i = 0; i < n; i++) {
+                int v = s[i];
+                int m = 0;
+                for (int j = prev[i].size() - 1; j >= 0; j--) {
+                    int u = prev[i].get(j);
+                    if (bo.bitAt(mask[v], u) == 1) {
+                        cnts[m]++;
+                        cnts[m | (1 << u)]--;
+                        cnts[m | (1 << v)]--;
+                        cnts[m | (1 << u) | (1 << v)]++;
+                    }
+                    m = bo.setBit(m, u, true);
+                }
+                m = 0;
+                for (int j = next[i].size() - 1; j >= 0; j--) {
+                    int u = next[i].get(j);
+                    if (bo.bitAt(mask[v], u) == 1) {
+                        cnts[m]++;
+                        cnts[m | (1 << u)]--;
+                        cnts[m | (1 << v)]--;
+                        cnts[m | (1 << u) | (1 << v)]++;
+                    }
+                    m = bo.setBit(m, u, true);
                 }
             }
 
-
-            if (!valid) {
-                out.println(0);
-                return;
+            FastWalshHadamardTransform.orFWT(cnts, 0, cnts.length - 1);
+            charCnt = new int[p];
+            for (int i = 0; i < n; i++) {
+                charCnt[s[i]]++;
             }
-
-            int ans = 1;
-            for (int i = 1; i <= n; i++) {
-                if (visited[i] || color[i] != 0) {
-                    continue;
-                }
-                dfs(i, 0);
-                ans = mod.mul(ans, dp[i][1]);
-            }
-
+            dp = new int[1 << p];
+            SequenceUtils.deepFill(dp, -1);
+            int ans = dp(0);
             out.println(ans);
         }
 
-        public void dfsForParent(int root, int p) {
-            parent[root] = p;
-            for (IntIterator iterator = edges.iterator(root); iterator.hasNext(); ) {
-                int node = iterator.next();
-                if (node == p) {
-                    continue;
+        public int dp(int s) {
+            if (dp[s] == -1) {
+                dp[s] = 0;
+                if (cnts[s] > 0) {
+                    return dp[s] = (int) 1e8;
                 }
-                dfsForParent(node, root);
-            }
-        }
-
-        public void paint(int root, int ancestor, int c) {
-            if (color[root] != 0) {
-                if (color[root] == c) {
-                    return;
+                for (int i = 0; i < p; i++) {
+                    if (bo.bitAt(s, i) == 0) {
+                        dp[s] += charCnt[i];
+                    }
                 }
-                valid = false;
-                return;
-            }
-            color[root] = c;
-            if (root != ancestor) {
-                paint(parent[root], ancestor, c);
-            }
-        }
-
-        public void dfs(int root, int p) {
-            if (visited[root]) {
-                return;
-            }
-            visited[root] = true;
-            if (color[root] != 0) {
-                dp[root][0] = 0;
-                dp[root][1] = 1;
-                return;
-            }
-            dp[root][0] = 1;
-            dp[root][1] = 0;
-            for (IntIterator iterator = edges.iterator(root); iterator.hasNext(); ) {
-                int node = iterator.next();
-                if (node == p) {
-                    continue;
-                }
-                dfs(node, root);
-                int total = mod.plus(dp[node][0], dp[node][1]);
-                dp[root][1] = mod.plus(mod.mul(dp[root][1], total), mod.mul(dp[root][0], dp[node][1]));
-                dp[root][0] = mod.mul(dp[root][0], total);
-            }
-        }
-
-    }
-
-    static class MultiWayIntStack {
-        private int[] values;
-        private int[] next;
-        private int[] heads;
-        private int alloc;
-        private int stackNum;
-
-        public IntIterator iterator(final int queue) {
-            return new IntIterator() {
-                int ele = heads[queue];
-
-
-                public boolean hasNext() {
-                    return ele != 0;
-                }
-
-
-                public int next() {
-                    int ans = values[ele];
-                    ele = next[ele];
-                    return ans;
-                }
-            };
-        }
-
-        private void doubleCapacity() {
-            int newSize = Math.max(next.length + 10, next.length * 2);
-            next = Arrays.copyOf(next, newSize);
-            values = Arrays.copyOf(values, newSize);
-        }
-
-        public void alloc() {
-            alloc++;
-            if (alloc >= next.length) {
-                doubleCapacity();
-            }
-            next[alloc] = 0;
-        }
-
-        public int stackNumber() {
-            return stackNum;
-        }
-
-        public MultiWayIntStack(int qNum, int totalCapacity) {
-            values = new int[totalCapacity + 1];
-            next = new int[totalCapacity + 1];
-            heads = new int[qNum];
-            stackNum = qNum;
-        }
-
-        public void addLast(int qId, int x) {
-            alloc();
-            values[alloc] = x;
-            next[alloc] = heads[qId];
-            heads[qId] = alloc;
-        }
-
-        public String toString() {
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < stackNum; i++) {
-                builder.append(i).append(": ");
-                for (IntIterator iterator = iterator(i); iterator.hasNext(); ) {
-                    builder.append(iterator.next()).append(",");
-                }
-                if (builder.charAt(builder.length() - 1) == ',') {
-                    builder.setLength(builder.length() - 1);
-                }
-                builder.append('\n');
-            }
-            return builder.toString();
-        }
-
-    }
-
-    static class Modular {
-        int m;
-
-        public Modular(int m) {
-            this.m = m;
-        }
-
-        public Modular(long m) {
-            this.m = (int) m;
-            if (this.m != m) {
-                throw new IllegalArgumentException();
-            }
-        }
-
-        public Modular(double m) {
-            this.m = (int) m;
-            if (this.m != m) {
-                throw new IllegalArgumentException();
-            }
-        }
-
-        public int valueOf(int x) {
-            x %= m;
-            if (x < 0) {
-                x += m;
-            }
-            return x;
-        }
-
-        public int valueOf(long x) {
-            x %= m;
-            if (x < 0) {
-                x += m;
-            }
-            return (int) x;
-        }
-
-        public int mul(int x, int y) {
-            return valueOf((long) x * y);
-        }
-
-        public int plus(int x, int y) {
-            return valueOf(x + y);
-        }
-
-        public String toString() {
-            return "mod " + m;
-        }
-
-    }
-
-    static class LcaOnTree {
-        int[] parent;
-        int[] preOrder;
-        int[] I;
-        int[] head;
-        int[] A;
-        int time;
-
-        void dfs1(MultiWayIntStack tree, int u, int p) {
-            parent[u] = p;
-            I[u] = preOrder[u] = time++;
-            for (IntIterator iterator = tree.iterator(u); iterator.hasNext(); ) {
-                int v = iterator.next();
-                if (v == p) continue;
-                dfs1(tree, v, u);
-                if (Integer.lowestOneBit(I[u]) < Integer.lowestOneBit(I[v])) {
-                    I[u] = I[v];
+                for (int i = 0; i < p; i++) {
+                    if (bo.bitAt(s, i) == 0) {
+                        dp[s] = Math.min(dp[s], dp(bo.setBit(s, i, true)));
+                    }
                 }
             }
-            head[I[u]] = u;
-        }
-
-        void dfs2(MultiWayIntStack tree, int u, int p, int up) {
-            A[u] = up | Integer.lowestOneBit(I[u]);
-            for (IntIterator iterator = tree.iterator(u); iterator.hasNext(); ) {
-                int v = iterator.next();
-                if (v == p) continue;
-                dfs2(tree, v, u, A[u]);
-            }
-        }
-
-        public LcaOnTree(MultiWayIntStack tree, int root) {
-            int n = tree.stackNumber();
-            preOrder = new int[n];
-            I = new int[n];
-            head = new int[n];
-            A = new int[n];
-            parent = new int[n];
-
-            dfs1(tree, root, -1);
-            dfs2(tree, root, -1, 0);
-        }
-
-        private int enterIntoStrip(int x, int hz) {
-            if (Integer.lowestOneBit(I[x]) == hz)
-                return x;
-            int hw = Integer.highestOneBit(A[x] & (hz - 1));
-            return parent[head[I[x] & -hw | hw]];
-        }
-
-        public int lca(int x, int y) {
-            int hb = I[x] == I[y] ? Integer.lowestOneBit(I[x]) : Integer.highestOneBit(I[x] ^ I[y]);
-            int hz = Integer.lowestOneBit(A[x] & A[y] & -hb);
-            int ex = enterIntoStrip(x, hz);
-            int ey = enterIntoStrip(y, hz);
-            return preOrder[ex] < preOrder[ey] ? ex : ey;
-        }
-
-    }
-
-    static interface IntIterator {
-        boolean hasNext();
-
-        int next();
-
-    }
-
-    static class FastOutput implements AutoCloseable, Closeable {
-        private StringBuilder cache = new StringBuilder(10 << 20);
-        private final Writer os;
-
-        public FastOutput(Writer os) {
-            this.os = os;
-        }
-
-        public FastOutput(OutputStream os) {
-            this(new OutputStreamWriter(os));
-        }
-
-        public FastOutput println(int c) {
-            cache.append(c).append('\n');
-            return this;
-        }
-
-        public FastOutput flush() {
-            try {
-                os.append(cache);
-                os.flush();
-                cache.setLength(0);
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-            return this;
-        }
-
-        public void close() {
-            flush();
-            try {
-                os.close();
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        }
-
-        public String toString() {
-            return cache.toString();
+            return dp[s];
         }
 
     }
@@ -443,6 +194,232 @@ public class Main {
             }
 
             return val;
+        }
+
+        public char readChar() {
+            skipBlank();
+            char c = (char) next;
+            next = read();
+            return c;
+        }
+
+    }
+
+    static class BitOperator {
+        public int bitAt(int x, int i) {
+            return (x >> i) & 1;
+        }
+
+        public int setBit(int x, int i, boolean v) {
+            if (v) {
+                x |= 1 << i;
+            } else {
+                x &= ~(1 << i);
+            }
+            return x;
+        }
+
+    }
+
+    static class IntList {
+        private int size;
+        private int cap;
+        private int[] data;
+        private static final int[] EMPTY = new int[0];
+
+        public IntList(int cap) {
+            this.cap = cap;
+            if (cap == 0) {
+                data = EMPTY;
+            } else {
+                data = new int[cap];
+            }
+        }
+
+        public IntList(IntList list) {
+            this.size = list.size;
+            this.cap = list.cap;
+            this.data = Arrays.copyOf(list.data, size);
+        }
+
+        public IntList() {
+            this(0);
+        }
+
+        public void ensureSpace(int req) {
+            if (req > cap) {
+                while (cap < req) {
+                    cap = Math.max(cap + 10, 2 * cap);
+                }
+                data = Arrays.copyOf(data, cap);
+            }
+        }
+
+        private void checkRange(int i) {
+            if (i < 0 || i >= size) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
+        }
+
+        public int get(int i) {
+            checkRange(i);
+            return data[i];
+        }
+
+        public void add(int x) {
+            ensureSpace(size + 1);
+            data[size++] = x;
+        }
+
+        public void addAll(int[] x, int offset, int len) {
+            ensureSpace(size + len);
+            System.arraycopy(x, offset, data, size, len);
+            size += len;
+        }
+
+        public void addAll(IntList list) {
+            addAll(list.data, 0, list.size);
+        }
+
+        public int indexOf(int x) {
+            for (int i = 0; i < size; i++) {
+                if (x == data[i]) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int pop() {
+            return data[--size];
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public void remove(int index) {
+            checkRange(index);
+            if (index == size - 1) {
+                pop();
+                return;
+            }
+            System.arraycopy(data, index + 1, data, index, size - index);
+            size--;
+        }
+
+        public int[] toArray() {
+            return Arrays.copyOf(data, size);
+        }
+
+        public String toString() {
+            return Arrays.toString(toArray());
+        }
+
+        public boolean equals(Object obj) {
+            if (!(obj instanceof IntList)) {
+                return false;
+            }
+            IntList other = (IntList) obj;
+            return SequenceUtils.equal(data, 0, size - 1, other.data, 0, other.size - 1);
+        }
+
+        public int hashCode() {
+            int h = 1;
+            for (int i = 0; i < size; i++) {
+                h = h * 31 + data[i];
+            }
+            return h;
+        }
+
+    }
+
+    static class FastOutput implements AutoCloseable, Closeable {
+        private StringBuilder cache = new StringBuilder(10 << 20);
+        private final Writer os;
+
+        public FastOutput(Writer os) {
+            this.os = os;
+        }
+
+        public FastOutput(OutputStream os) {
+            this(new OutputStreamWriter(os));
+        }
+
+        public FastOutput println(int c) {
+            cache.append(c).append('\n');
+            return this;
+        }
+
+        public FastOutput flush() {
+            try {
+                os.append(cache);
+                os.flush();
+                cache.setLength(0);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+            return this;
+        }
+
+        public void close() {
+            flush();
+            try {
+                os.close();
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+
+        public String toString() {
+            return cache.toString();
+        }
+
+    }
+
+    static class FastWalshHadamardTransform {
+        public static void orFWT(int[] p, int l, int r) {
+            if (l == r) {
+                return;
+            }
+            int m = (l + r) >> 1;
+            orFWT(p, l, m);
+            orFWT(p, m + 1, r);
+            for (int i = 0, until = m - l; i <= until; i++) {
+                int a = p[l + i];
+                int b = p[m + 1 + i];
+                p[m + 1 + i] = a + b;
+            }
+        }
+
+    }
+
+    static class SequenceUtils {
+        public static void deepFill(Object array, int val) {
+            if (!array.getClass().isArray()) {
+                throw new IllegalArgumentException();
+            }
+            if (array instanceof int[]) {
+                int[] intArray = (int[]) array;
+                Arrays.fill(intArray, val);
+            } else {
+                Object[] objArray = (Object[]) array;
+                for (Object obj : objArray) {
+                    deepFill(obj, val);
+                }
+            }
+        }
+
+        public static boolean equal(int[] a, int al, int ar, int[] b, int bl, int br) {
+            if ((ar - al) != (br - bl)) {
+                return false;
+            }
+            for (int i = al, j = bl; i <= ar; i++, j++) {
+                if (a[i] != b[j]) {
+                    return false;
+                }
+            }
+            return true;
         }
 
     }
---
+++ src/test/java/on2019_12/on2019_12_06_Educational_Codeforces_Round_60__Rated_for_Div__2_/F___Crisp_String/Main.java
@@ -0,0 +1,14 @@
+package on2019_12.on2019_12_06_Educational_Codeforces_Round_60__Rated_for_Div__2_.F___Crisp_String;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("src/test/java/on2019_12/on2019_12_06_Educational_Codeforces_Round_60__Rated_for_Div__2_/F___Crisp_String/F - Crisp String.json"))
+			Assert.fail();
+	}
+}
