3a575c984b8271588c38c104e6fe878b8bb08407
taodaling <taodaling@gmail.com>
2019-12-11 23:12:37 +0800

    solve more and more, will I be stronger

--- output/Main.java
+++ output/Main.java
@@ -2,12 +2,10 @@ import java.io.OutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.io.IOException;
-import java.util.stream.Collectors;
 import java.io.UncheckedIOException;
-import java.util.List;
-import java.util.stream.Stream;
-import java.util.Map;
 import java.io.Closeable;
 import java.io.Writer;
 import java.io.OutputStreamWriter;
@@ -31,89 +29,185 @@ public class Main {
             OutputStream outputStream = System.out;
             FastInput in = new FastInput(inputStream);
             FastOutput out = new FastOutput(outputStream);
-            FTrucksAndCities solver = new FTrucksAndCities();
+            FIvanAndBurgers solver = new FIvanAndBurgers();
             solver.solve(1, in, out);
             out.close();
         }
     }
 
-    static class FTrucksAndCities {
+    static class FIvanAndBurgers {
         public void solve(int testNumber, FastInput in, FastOutput out) {
             int n = in.readInt();
-            int m = in.readInt();
-            int[] a = new int[n];
+            int[] c = new int[n];
             for (int i = 0; i < n; i++) {
-                a[i] = in.readInt();
+                c[i] = in.readInt();
             }
-            Car[] cars = new Car[m];
-            for (int i = 0; i < m; i++) {
-                cars[i] = new Car();
-                cars[i].s = in.readInt() - 1;
-                cars[i].f = in.readInt() - 1;
-                cars[i].c = in.readInt();
-                cars[i].r = in.readInt();
+            int qCnt = in.readInt();
+            Query[] qs = new Query[qCnt];
+            for (int i = 0; i < qCnt; i++) {
+                qs[i] = new Query();
+                qs[i].l = in.readInt() - 1;
+                qs[i].r = in.readInt() - 1;
             }
-            Map<Integer, List<Car>> carGroupByR =
-                    Stream.of(cars).collect(Collectors.groupingBy(x -> x.r));
-
-            int[][] lastDp = new int[n][n];
-            int[][] nextDp = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                for (int j = i; j < n; j++) {
-                    if (i == j) {
-                        lastDp[i][j] = 0;
-                    } else {
-                        lastDp[i][j] = a[j] - a[i];
-                    }
-                }
-            }
-
-            relax(lastDp, carGroupByR.get(0));
-            for (int i = 1; i <= n; i++) {
-                for (int j = 0; j < n; j++) {
-                    int ptr = j;
-                    for (int k = j; k < n; k++) {
-                        while (ptr < k && Math.max(lastDp[j][ptr], a[k] - a[ptr]) >=
-                                Math.max(lastDp[j][ptr + 1], a[k] - a[ptr + 1])) {
-                            ptr++;
+            Query[] qsSorted = qs.clone();
+            Arrays.sort(qsSorted, (a, b) -> a.l == b.l ? a.r - b.r : b.l - a.l);
+            SimplifiedDeque<Query> deque = new Array2DequeAdapter<>(qsSorted);
+            LinearBasis basis = new LinearBasis();
+            IntList last = new IntList();
+            IntList next = new IntList();
+            for (int i = n - 1; i >= 0; i--) {
+                basis.clear();
+                next.clear();
+                basis.add(c[i]);
+                next.add(i);
+                int j = 0;
+                while (!deque.isEmpty() && deque.peekFirst().l == i) {
+                    Query q = deque.removeFirst();
+                    while (j < last.size() && last.get(j) <= q.r) {
+                        next.add(last.get(j++));
+                        if (!basis.add(c[next.tail()])) {
+                            next.pop();
                         }
-                        nextDp[j][k] = Math.max(lastDp[j][ptr], a[j] - a[ptr]);
+                    }
+                    q.ans = basis.theMaximumNumberXor(0);
+                }
+                while (j < last.size()) {
+                    next.add(last.get(j++));
+                    if (!basis.add(c[next.tail()])) {
+                        next.pop();
                     }
                 }
-                relax(nextDp, carGroupByR.get(i));
-                {
-                    int[][] tmp = lastDp;
-                    lastDp = nextDp;
-                    nextDp = tmp;
-                }
+                IntList tmp = last;
+                last = next;
+                next = tmp;
             }
 
-            long ans = 0;
-            for (Car car : cars) {
-                ans = Math.max(ans, car.v);
-            }
-
-            out.println(ans);
-        }
-
-        public void relax(int[][] dp, List<Car> cars) {
-            if (cars == null) {
-                return;
-            }
-            for (Car car : cars) {
-                long stepAtLeast = (long) dp[car.s][car.f] * car.c;
-                car.v = stepAtLeast;
+            for (Query q : qs) {
+                out.println(q.ans);
             }
         }
 
     }
 
-    static class Car {
-        int s;
-        int f;
-        int c;
+    static class FastOutput implements AutoCloseable, Closeable {
+        private StringBuilder cache = new StringBuilder(10 << 20);
+        private final Writer os;
+
+        public FastOutput(Writer os) {
+            this.os = os;
+        }
+
+        public FastOutput(OutputStream os) {
+            this(new OutputStreamWriter(os));
+        }
+
+        public FastOutput println(int c) {
+            cache.append(c).append('\n');
+            return this;
+        }
+
+        public FastOutput flush() {
+            try {
+                os.append(cache);
+                os.flush();
+                cache.setLength(0);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+            return this;
+        }
+
+        public void close() {
+            flush();
+            try {
+                os.close();
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+
+        public String toString() {
+            return cache.toString();
+        }
+
+    }
+
+    static interface SimplifiedStack<T> extends Iterable<T> {
+        boolean isEmpty();
+
+    }
+
+    static interface SimplifiedDeque<T> extends SimplifiedStack<T> {
+        T peekFirst();
+
+        T removeFirst();
+
+    }
+
+    static class Array2DequeAdapter<T> implements SimplifiedDeque<T> {
+        T[] data;
+        int l;
         int r;
-        long v;
+
+        public Array2DequeAdapter(T[] data) {
+            this(data, 0, data.length - 1);
+        }
+
+        public Array2DequeAdapter(T[] data, int l, int r) {
+            this.data = data;
+            this.l = l;
+            this.r = r;
+        }
+
+        public boolean isEmpty() {
+            return l > r;
+        }
+
+        public T peekFirst() {
+            return data[l];
+        }
+
+        public T removeFirst() {
+            return data[l++];
+        }
+
+        public Iterator<T> iterator() {
+            return new Iterator<T>() {
+                int iter = l;
+
+
+                public boolean hasNext() {
+                    return iter <= r;
+                }
+
+
+                public T next() {
+                    return data[iter++];
+                }
+            };
+        }
+
+    }
+
+    static class SequenceUtils {
+        public static boolean equal(int[] a, int al, int ar, int[] b, int bl, int br) {
+            if ((ar - al) != (br - bl)) {
+                return false;
+            }
+            for (int i = al, j = bl; i <= ar; i++, j++) {
+                if (a[i] != b[j]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+
+    static class Log2 {
+        public int floorLog(int x) {
+            return 31 - Integer.numberOfLeadingZeros(x);
+        }
 
     }
 
@@ -176,45 +270,158 @@ public class Main {
 
     }
 
-    static class FastOutput implements AutoCloseable, Closeable {
-        private StringBuilder cache = new StringBuilder(10 << 20);
-        private final Writer os;
+    static class IntList {
+        private int size;
+        private int cap;
+        private int[] data;
+        private static final int[] EMPTY = new int[0];
 
-        public FastOutput(Writer os) {
-            this.os = os;
-        }
-
-        public FastOutput(OutputStream os) {
-            this(new OutputStreamWriter(os));
-        }
-
-        public FastOutput println(long c) {
-            cache.append(c).append('\n');
-            return this;
-        }
-
-        public FastOutput flush() {
-            try {
-                os.append(cache);
-                os.flush();
-                cache.setLength(0);
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
+        public IntList(int cap) {
+            this.cap = cap;
+            if (cap == 0) {
+                data = EMPTY;
+            } else {
+                data = new int[cap];
             }
-            return this;
         }
 
-        public void close() {
-            flush();
-            try {
-                os.close();
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
+        public IntList(IntList list) {
+            this.size = list.size;
+            this.cap = list.cap;
+            this.data = Arrays.copyOf(list.data, size);
+        }
+
+        public IntList() {
+            this(0);
+        }
+
+        public void ensureSpace(int req) {
+            if (req > cap) {
+                while (cap < req) {
+                    cap = Math.max(cap + 10, 2 * cap);
+                }
+                data = Arrays.copyOf(data, cap);
             }
+        }
+
+        private void checkRange(int i) {
+            if (i < 0 || i >= size) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
+        }
+
+        public int get(int i) {
+            checkRange(i);
+            return data[i];
+        }
+
+        public void add(int x) {
+            ensureSpace(size + 1);
+            data[size++] = x;
+        }
+
+        public int tail() {
+            checkRange(0);
+            return data[size - 1];
+        }
+
+        public int pop() {
+            return data[--size];
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public int[] toArray() {
+            return Arrays.copyOf(data, size);
+        }
+
+        public void clear() {
+            size = 0;
         }
 
         public String toString() {
-            return cache.toString();
+            return Arrays.toString(toArray());
+        }
+
+        public boolean equals(Object obj) {
+            if (!(obj instanceof IntList)) {
+                return false;
+            }
+            IntList other = (IntList) obj;
+            return SequenceUtils.equal(data, 0, size - 1, other.data, 0, other.size - 1);
+        }
+
+        public int hashCode() {
+            int h = 1;
+            for (int i = 0; i < size; i++) {
+                h = h * 31 + data[i];
+            }
+            return h;
+        }
+
+    }
+
+    static class Query {
+        int l;
+        int r;
+        int ans;
+
+    }
+
+    static class LinearBasis {
+        private int[] map = new int[20];
+        private int size;
+        private Log2 log2 = new Log2();
+
+        public void clear() {
+            size = 0;
+            Arrays.fill(map, 0);
+        }
+
+        private void afterAddBit(int bit) {
+            for (int i = 19; i >= 0; i--) {
+                if (i == bit || map[i] == 0) {
+                    continue;
+                }
+                if (bitAt(map[i], bit) == 1) {
+                    map[i] ^= map[bit];
+                }
+            }
+        }
+
+        public boolean add(int val) {
+            for (int i = 19; i >= 0 && val != 0; i--) {
+                if (bitAt(val, i) == 0) {
+                    continue;
+                }
+                val ^= map[i];
+            }
+            if (val != 0) {
+                int log = log2.floorLog(val);
+                map[log] = val;
+                size++;
+                afterAddBit(log);
+                return true;
+            }
+            return false;
+        }
+
+        private long bitAt(int val, int i) {
+            return (val >>> i) & 1;
+        }
+
+        public int theMaximumNumberXor(int x) {
+            for (int i = 0; i < 20; i++) {
+                if (map[i] == 0) {
+                    continue;
+                }
+                if (bitAt(x, i) == 0) {
+                    x ^= map[i];
+                }
+            }
+            return x;
         }
 
     }
---
+++ src/test/java/template/math/EulerSieveTest.java
@@ -0,0 +1,12 @@
+package template.math;
+
+import org.junit.Test;
+
+public class EulerSieveTest {
+
+    @Test
+    public void test(){
+        EulerSieve es = new EulerSieve(100);
+        return;
+    }
+}
