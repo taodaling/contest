121c53902feb8218d97f16e15605dc644bc444b1
taodaling <taodaling@gmail.com>
2019-12-12 22:54:47 +0800

    solve more and more, will I be stronger

---
+++ src/test/java/on2019_12/on2019_12_12_Codeforces_Round__530__Div__1_/D__Eels/Main.java
@@ -0,0 +1,14 @@
+package on2019_12.on2019_12_12_Codeforces_Round__530__Div__1_.D__Eels;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("src/test/java/on2019_12/on2019_12_12_Codeforces_Round__530__Div__1_/D__Eels/D. Eels.json"))
+			Assert.fail();
+	}
+}
