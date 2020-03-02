52859b6a2cf209b0abc4284df3efc96abcbeef48
taodaling <daling.tao@ideacome.com>
2020-03-02 18:05:58 +0800

    archive

---
+++ src/test/java/on2020_03/on2020_03_02_AtCoder_Regular_Contest_085/F___NRE/FNRE.java
@@ -0,0 +1,28 @@
+package on2020_03.on2020_03_02_AtCoder_Regular_Contest_085.F___NRE;
+
+
+
+import template.io.FastInput;
+import template.io.FastOutput;
+import template.problem.IntervalPickProblem;
+
+public class FNRE {
+    public void solve(int testNumber, FastInput in, FastOutput out) {
+        int n = in.readInt();
+        long[] val = new long[n + 1];
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            int x = in.readInt();
+            val[i] = x == 1 ? 1 : -1;
+            sum += x;
+        }
+        int q = in.readInt();
+        IntervalPickProblem.Interval[] intervals = new IntervalPickProblem.Interval[q];
+        for (int i = 0; i < q; i++) {
+            intervals[i] = new IntervalPickProblem.Interval(in.readInt(), in.readInt());
+        }
+        int minus = (int) IntervalPickProblem.solve(val, intervals);
+        sum -= minus;
+        out.println(sum);
+    }
+}
