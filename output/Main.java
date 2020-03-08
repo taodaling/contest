848f77dfbca117e37911e3fa4a8067317fc08d1f
taodaling <taodaling@gmail.com>
2020-03-09 01:07:16 +0800

    contest

--- output/Main.java
+++ output/Main.java
@@ -2,17 +2,17 @@ import java.io.OutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
-import java.util.Deque;
 import java.util.ArrayList;
+import java.util.Map;
 import java.io.OutputStreamWriter;
 import java.io.OutputStream;
-import java.util.Collection;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.List;
+import java.util.TreeMap;
 import java.io.Closeable;
+import java.util.Map.Entry;
 import java.io.Writer;
-import java.util.ArrayDeque;
 import java.io.InputStream;
 
 /**
@@ -33,44 +33,67 @@ public class Main {
             OutputStream outputStream = System.out;
             FastInput in = new FastInput(inputStream);
             FastOutput out = new FastOutput(outputStream);
-            Domino solver = new Domino();
+            DMangaMarket solver = new DMangaMarket();
             solver.solve(1, in, out);
             out.close();
         }
     }
 
-    static class Domino {
+    static class DMangaMarket {
         public void solve(int testNumber, FastInput in, FastOutput out) {
             int n = in.readInt();
-            IntegerList[][] edges = new IntegerList[7][7];
-            for (int i = 0; i < 7; i++) {
-                for (int j = 0; j < 7; j++) {
-                    edges[i][j] = new IntegerList();
-                }
-            }
-            UndirectedEulerTrace trace = new UndirectedEulerTrace(7, n);
+            long t = in.readInt();
+            List<Store> nonZero = new ArrayList<>(n);
+            List<Store> zero = new ArrayList<>(n);
+
             for (int i = 0; i < n; i++) {
                 int a = in.readInt();
                 int b = in.readInt();
-                edges[a][b].add(i + 1);
-                trace.addEdge(a, b);
+                Store store = new Store();
+                store.a = a;
+                store.b = b;
+                if (a == 0) {
+                    zero.add(store);
+                } else {
+                    nonZero.add(store);
+                }
             }
 
-            if (!trace.findTrace()) {
-                out.println("No solution");
-                return;
-            }
-            IntegerList et = trace.getEulerTrace();
-            for (int i = 1; i < et.size(); i++) {
-                int a = et.get(i - 1);
-                int b = et.get(i);
-                if (!edges[a][b].isEmpty()) {
-                    out.append(edges[a][b].pop()).append(' ').append('+');
-                } else {
-                    out.append(edges[b][a].pop()).append(' ').append('-');
+            zero.sort((a, b) -> Long.compare(a.b, b.b));
+            nonZero.sort((a, b) -> Long.compare((a.b + 1) * b.a, (b.b + 1) * a.a));
+            int m = nonZero.size();
+            long[][] dp = new long[m + 1][30];
+            long inf = (long) 1e18;
+            SequenceUtils.deepFill(dp, inf);
+            dp[0][0] = 0;
+            for (int i = 1; i <= m; i++) {
+                Store s = nonZero.get(i - 1);
+                for (int j = 0; j < 30; j++) {
+                    dp[i][j] = dp[i - 1][j];
+                    if (j > 0) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1] + 1 + DigitUtils.mul(dp[i - 1][j - 1] + 1, s.a, inf, inf) + s.b);
+                    }
                 }
-                out.println();
             }
+
+            long sum = 0;
+            TreeMap<Long, Integer> map = new TreeMap<>();
+            for (int i = 0; i < zero.size(); i++) {
+                Store s = zero.get(i);
+                sum += s.b + 1;
+                map.put(sum, i + 1);
+            }
+
+            long ans = 0;
+            for (int i = 0; i < 30; i++) {
+                if (dp[m][i] > t) {
+                    continue;
+                }
+                Map.Entry<Long, Integer> floor = map.floorEntry(t - dp[m][i]);
+                ans = Math.max(ans, i + (floor == null ? 0 : floor.getValue()));
+            }
+
+            out.println(ans);
         }
 
     }
@@ -134,233 +157,36 @@ public class Main {
 
     }
 
-    static class IntegerList implements Cloneable {
-        private int size;
-        private int cap;
-        private int[] data;
-        private static final int[] EMPTY = new int[0];
+    static class DigitUtils {
+        private DigitUtils() {
+        }
 
-        public IntegerList(int cap) {
-            this.cap = cap;
-            if (cap == 0) {
-                data = EMPTY;
-            } else {
-                data = new int[cap];
+        public static boolean isMultiplicationOverflow(long a, long b, long limit) {
+            if (limit < 0) {
+                limit = -limit;
             }
-        }
-
-        public IntegerList(IntegerList list) {
-            this.size = list.size;
-            this.cap = list.cap;
-            this.data = Arrays.copyOf(list.data, size);
-        }
-
-        public IntegerList() {
-            this(0);
-        }
-
-        public void ensureSpace(int req) {
-            if (req > cap) {
-                while (cap < req) {
-                    cap = Math.max(cap + 10, 2 * cap);
-                }
-                data = Arrays.copyOf(data, cap);
+            if (a < 0) {
+                a = -a;
             }
-        }
-
-        private void checkRange(int i) {
-            if (i < 0 || i >= size) {
-                throw new ArrayIndexOutOfBoundsException();
+            if (b < 0) {
+                b = -b;
             }
-        }
-
-        public int get(int i) {
-            checkRange(i);
-            return data[i];
-        }
-
-        public void add(int x) {
-            ensureSpace(size + 1);
-            data[size++] = x;
-        }
-
-        public void addAll(int[] x, int offset, int len) {
-            ensureSpace(size + len);
-            System.arraycopy(x, offset, data, size, len);
-            size += len;
-        }
-
-        public void addAll(IntegerList list) {
-            addAll(list.data, 0, list.size);
-        }
-
-        public int pop() {
-            return data[--size];
-        }
-
-        public int size() {
-            return size;
-        }
-
-        public int[] toArray() {
-            return Arrays.copyOf(data, size);
-        }
-
-        public boolean isEmpty() {
-            return size == 0;
-        }
-
-        public void clear() {
-            size = 0;
-        }
-
-        public String toString() {
-            return Arrays.toString(toArray());
-        }
-
-        public boolean equals(Object obj) {
-            if (!(obj instanceof IntegerList)) {
+            if (a == 0 || b == 0) {
                 return false;
             }
-            IntegerList other = (IntegerList) obj;
-            return SequenceUtils.equal(data, 0, size - 1, other.data, 0, other.size - 1);
+            //a * b > limit => a > limit / b
+            return a > limit / b;
         }
 
-        public int hashCode() {
-            int h = 1;
-            for (int i = 0; i < size; i++) {
-                h = h * 31 + Integer.hashCode(data[i]);
-            }
-            return h;
-        }
-
-        public IntegerList clone() {
-            IntegerList ans = new IntegerList();
-            ans.addAll(this);
-            return ans;
+        public static long mul(long a, long b, long limit, long overflowVal) {
+            return isMultiplicationOverflow(a, b, limit) ? overflowVal : a * b;
         }
 
     }
 
-    static class UndirectedEulerTrace {
-        UndirectedEulerTrace.Node[] nodes;
-        List<UndirectedEulerTrace.Edge> edges;
-        boolean containEulerTrace;
-        boolean isEulerTraceClose;
-        IntegerList eulerTrace;
-
-        public UndirectedEulerTrace(int n, int m) {
-            nodes = new UndirectedEulerTrace.Node[n];
-            for (int i = 0; i < n; i++) {
-                nodes[i] = new UndirectedEulerTrace.Node();
-                nodes[i].id = i;
-            }
-            edges = new ArrayList<>(m);
-            eulerTrace = new IntegerList(m);
-        }
-
-        public void addEdge(int a, int b) {
-            UndirectedEulerTrace.Edge edge = new UndirectedEulerTrace.Edge();
-            edge.a = nodes[a];
-            edge.b = nodes[b];
-            nodes[a].edges.add(edge);
-            nodes[b].edges.add(edge);
-            edges.add(edge);
-        }
-
-        public IntegerList getEulerTrace() {
-            return eulerTrace;
-        }
-
-        private void dfs(UndirectedEulerTrace.Node root) {
-            while (!root.deque.isEmpty()) {
-                UndirectedEulerTrace.Edge tail = root.deque.removeFirst();
-                if (tail.visited) {
-                    continue;
-                }
-                tail.visited = true;
-                dfs(tail.other(root));
-            }
-            eulerTrace.add(root.id);
-        }
-
-        public boolean findEulerTraceSince(UndirectedEulerTrace.Node root) {
-            eulerTrace.clear();
-            containEulerTrace = false;
-            isEulerTraceClose = false;
-            for (UndirectedEulerTrace.Node node : nodes) {
-                node.deque.clear();
-                node.deque.addAll(node.edges);
-            }
-            dfs(root);
-            containEulerTrace = eulerTrace.size() == edges.size() + 1;
-            if (!containEulerTrace) {
-                return false;
-            }
-            isEulerTraceClose = eulerTrace.get(eulerTrace.size() - 1)
-                    == eulerTrace.get(0);
-            return true;
-        }
-
-        public boolean findTrace() {
-            eulerTrace.clear();
-            containEulerTrace = false;
-            isEulerTraceClose = false;
-
-            UndirectedEulerTrace.Node p1 = null;
-            UndirectedEulerTrace.Node p2 = null;
-            for (UndirectedEulerTrace.Node node : nodes) {
-                if (node.edges.size() % 2 != 0) {
-                    if (p1 == null) {
-                        p1 = node;
-                    } else if (p2 == null) {
-                        p2 = node;
-                    } else {
-                        containEulerTrace = false;
-                        return false;
-                    }
-                }
-            }
-
-            if (p1 != null && p2 != null) {
-                if (findEulerTraceSince(p1)) {
-                    return true;
-                }
-                if (findEulerTraceSince(p2)) {
-                    return true;
-                }
-            }
-
-            for (UndirectedEulerTrace.Node node : nodes) {
-                if (node.edges.size() > 0) {
-                    return findEulerTraceSince(node);
-                }
-            }
-
-            return true;
-        }
-
-        private static class Node {
-            List<UndirectedEulerTrace.Edge> edges = new ArrayList<>(2);
-            Deque<UndirectedEulerTrace.Edge> deque = new ArrayDeque<>();
-            int id;
-
-            public String toString() {
-                return "" + id;
-            }
-
-        }
-
-        private static class Edge {
-            UndirectedEulerTrace.Node a;
-            UndirectedEulerTrace.Node b;
-            boolean visited;
-
-            public UndirectedEulerTrace.Node other(UndirectedEulerTrace.Node x) {
-                return a == x ? b : a;
-            }
-
-        }
+    static class Store {
+        long a;
+        long b;
 
     }
 
@@ -391,17 +217,12 @@ public class Main {
             return this;
         }
 
-        public FastOutput append(int c) {
+        public FastOutput append(long c) {
             cache.append(c);
             return this;
         }
 
-        public FastOutput append(String c) {
-            cache.append(c);
-            return this;
-        }
-
-        public FastOutput println(String c) {
+        public FastOutput println(long c) {
             return append(c).println();
         }
 
@@ -437,16 +258,19 @@ public class Main {
     }
 
     static class SequenceUtils {
-        public static boolean equal(int[] a, int al, int ar, int[] b, int bl, int br) {
-            if ((ar - al) != (br - bl)) {
-                return false;
+        public static void deepFill(Object array, long val) {
+            if (!array.getClass().isArray()) {
+                throw new IllegalArgumentException();
             }
-            for (int i = al, j = bl; i <= ar; i++, j++) {
-                if (a[i] != b[j]) {
-                    return false;
+            if (array instanceof long[]) {
+                long[] longArray = (long[]) array;
+                Arrays.fill(longArray, val);
+            } else {
+                Object[] objArray = (Object[]) array;
+                for (Object obj : objArray) {
+                    deepFill(obj, val);
                 }
             }
-            return true;
         }
 
     }
