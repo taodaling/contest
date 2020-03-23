45c2a31bcb78595f8ae1a821883402dea648dbe4
taodaling <taodaling@gmail.com>
2020-03-24 01:28:07 +0800

    solve more

---
+++ src/test/java/on2020_03/on2020_03_24_Educational_Codeforces_Round_84__Rated_for_Div__2_/G__Letters_and_Question_Marks/Main.java
@@ -0,0 +1,14 @@
+package on2020_03.on2020_03_24_Educational_Codeforces_Round_84__Rated_for_Div__2_.G__Letters_and_Question_Marks;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("src/test/java/on2020_03/on2020_03_24_Educational_Codeforces_Round_84__Rated_for_Div__2_/G__Letters_and_Question_Marks/G. Letters and Question Marks.json"))
+			Assert.fail();
+	}
+}
