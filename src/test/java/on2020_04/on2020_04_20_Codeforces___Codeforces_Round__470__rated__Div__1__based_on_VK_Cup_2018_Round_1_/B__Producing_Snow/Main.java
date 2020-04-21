58adef0d8ed6239a7bb9fe94bb062e24e50cf4b2
taodaling <taodaling@gmail.com>
2020-04-22 01:06:49 +0800

    solve more

---
+++ src/test/java/on2020_04/on2020_04_20_Codeforces___Codeforces_Round__470__rated__Div__1__based_on_VK_Cup_2018_Round_1_/B__Producing_Snow/Main.java
@@ -0,0 +1,14 @@
+package on2020_04.on2020_04_20_Codeforces___Codeforces_Round__470__rated__Div__1__based_on_VK_Cup_2018_Round_1_.B__Producing_Snow;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("src/test/java/on2020_04/on2020_04_20_Codeforces___Codeforces_Round__470__rated__Div__1__based_on_VK_Cup_2018_Round_1_/B__Producing_Snow/B. Producing Snow.json"))
+			Assert.fail();
+	}
+}
---
+++ src/test/java/on2020_04/on2020_04_21_TopCoder_SRM__767/MahdiJumping/MahdiJumping.java
@@ -0,0 +1,45 @@
+package on2020_04.on2020_04_21_TopCoder_SRM__767.MahdiJumping;
+
+
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class MahdiJumping {
+    public long minDis(int n, int A, int B, int a, int b) {
+        long inf = (long) 1e18;
+        Node[] nodes = new Node[n];
+        for (int i = 0; i < n; i++) {
+            nodes[i] = new Node();
+            nodes[i].index = i;
+            nodes[i].dist = inf;
+        }
+
+        nodes[0].dist = 0;
+        TreeSet<Node> set = new TreeSet<Node>((x, y) -> x.dist == y.dist ? Integer.compare(x.index, y.index) :
+                Long.compare(x.dist, y.dist));
+        set.add(nodes[0]);
+        while (!set.isEmpty()) {
+            Node head = set.pollFirst();
+            if (head.index + 1 < n && nodes[head.index + 1].dist >
+                    head.dist + a) {
+                set.remove(nodes[head.index + 1]);
+                nodes[head.index + 1].dist = head.dist + a;
+                set.add(nodes[head.index + 1]);
+            }
+            Node next = nodes[(int) (((long) A * head.index + B) % n)];
+            if (next.dist > head.dist + b) {
+                set.remove(next);
+                next.dist = head.dist + b;
+                set.add(next);
+            }
+        }
+
+        return nodes[n - 1].dist;
+    }
+}
+
+class Node {
+    int index;
+    long dist;
+}
