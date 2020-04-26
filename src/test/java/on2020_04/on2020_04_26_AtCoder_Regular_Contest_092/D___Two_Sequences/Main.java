3cfaf6261adb636529b7b25976af9158d057060e
taodaling <daling.tao@ideacome.com>
2020-04-26 17:54:11 +0800

    solve more

---
+++ src/test/java/on2020_04/on2020_04_26_AtCoder_Regular_Contest_092/D___Two_Sequences/Main.java
@@ -0,0 +1,14 @@
+package on2020_04.on2020_04_26_AtCoder_Regular_Contest_092.D___Two_Sequences;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("src/test/java/on2020_04/on2020_04_26_AtCoder_Regular_Contest_092/D___Two_Sequences/D - Two Sequences.json"))
+			Assert.fail();
+	}
+}
---
+++ src/test/java/on2020_04/on2020_04_26_AtCoder_Regular_Contest_092/D___Two_Sequences/TaskD.java
@@ -0,0 +1,74 @@
+package on2020_04.on2020_04_26_AtCoder_Regular_Contest_092.D___Two_Sequences;
+
+
+
+import template.binary.Bits;
+import template.io.FastInput;
+import template.io.FastOutput;
+import template.utils.CompareUtils;
+import template.utils.Debug;
+
+public class TaskD {
+    Debug debug = new Debug(false);
+    public void solve(int testNumber, FastInput in, FastOutput out) {
+        int n = in.readInt();
+        int[] A = new int[n];
+        int[] B = new int[n];
+        for (int i = 0; i < n; i++) {
+            A[i] = in.readInt();
+        }
+        for (int i = 0; i < n; i++) {
+            B[i] = in.readInt();
+        }
+        long ans = 0;
+        for (int i = 0; i < 29; i++) {
+            int mask = (1 << (i + 1)) - 1;
+            CompareUtils.radixSort(A, 0, n - 1, a -> a & mask);
+            CompareUtils.radixSort(B, 0, n - 1, b -> b & mask);
+            int am = separate(A, 0, n - 1, 1 << i);
+            int bm = separate(B, 0, n - 1, 1 << i);
+
+            long cnt = countNotExceed(A, 0, am, B, bm + 1, n - 1, mask >>> 1)
+                    + countNotExceed(A, am + 1, n - 1, B, 0, bm, mask >>> 1)
+                    + countExceed(A, 0, am, B, 0, bm, mask >>> 1)
+                    + countExceed(A, am + 1, n - 1, B, bm + 1, n - 1, mask >>> 1);
+
+            ans = Bits.setBit(ans, i, (cnt % 2) == 1);
+            debug.debug("cnt", cnt);
+        }
+
+        out.println(ans);
+    }
+
+    public int separate(int[] A, int l, int r, int bit) {
+        int am = l - 1;
+        while (am + 1 <= r && (A[am + 1] & bit) == 0) {
+            am++;
+        }
+        return am;
+    }
+
+    public long countExceed(int[] a, int al, int ar, int[] b, int bl, int br, int mask) {
+        int l = br + 1;
+        long ans = 0;
+        for (int i = al; i <= ar; i++) {
+            while (l > bl && (b[l - 1] & mask) + (a[i] & mask) > mask) {
+                l--;
+            }
+            ans += br - l + 1;
+        }
+        return ans;
+    }
+
+    public long countNotExceed(int[] a, int al, int ar, int[] b, int bl, int br, int mask) {
+        int l = br;
+        long ans = 0;
+        for (int i = al; i <= ar; i++) {
+            while (l >= bl && (b[l] & mask) + (a[i] & mask) > mask) {
+                l--;
+            }
+            ans += l - bl + 1;
+        }
+        return ans;
+    }
+}
