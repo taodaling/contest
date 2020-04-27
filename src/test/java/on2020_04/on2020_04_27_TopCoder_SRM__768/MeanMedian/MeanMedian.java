6eccbbe0c715e21191a3629f53b03dbc23f9b145
taodaling <taodaling@gmail.com>
2020-04-28 01:24:45 +0800

    solve more

---
+++ src/test/java/on2020_04/on2020_04_27_TopCoder_SRM__768/MeanMedian/MeanMedian.java
@@ -0,0 +1,38 @@
+package on2020_04.on2020_04_27_TopCoder_SRM__768.MeanMedian;
+
+
+
+import template.utils.Debug;
+
+import java.util.Arrays;
+
+public class MeanMedian {
+//
+//    Debug debug = new Debug(true);
+    public int minEffort(int needMean, int needMedian, int[] d) {
+        int n = d.length;
+        int sum = needMean * n;
+        Arrays.sort(d);
+        int half = (n + 1) / 2;
+        int[] scores = new int[n];
+        int ans = 0;
+        int curSum = 0;
+        for (int i = 0; i < half; i++) {
+            scores[i] = needMedian;
+            curSum += needMedian;
+            ans += needMedian * d[i];
+        }
+
+        for (int i = 0; i < n && curSum < sum; i++) {
+            int add = Math.min(10 - scores[i], sum - curSum);
+            scores[i] += add;
+            curSum += add;
+            ans += add * d[i];
+        }
+
+//
+//        debug.debug("d", d);
+//        debug.debug("scores", scores);
+        return ans;
+    }
+}
