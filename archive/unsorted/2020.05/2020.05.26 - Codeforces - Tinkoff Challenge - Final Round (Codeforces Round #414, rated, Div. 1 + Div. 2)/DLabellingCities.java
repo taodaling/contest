01b6a05421c6263d845ee5770f5713215ba2526c
taodaling <daling.tao@ideacome.com>
2020-05-26 14:11:51 +0800

    solve more

---
+++ archive/unsorted/2020.05/2020.05.26 - Codeforces - Tinkoff Challenge - Final Round (Codeforces Round #414, rated, Div. 1 + Div. 2)/DLabellingCities.java
@@ -0,0 +1,127 @@
+package contest;
+
+import template.io.FastInput;
+import template.io.FastOutput;
+import template.math.DigitUtils;
+import template.math.Modular;
+import template.primitve.generated.datastructure.LongObjectHashMap;
+import template.rand.HashData;
+import template.rand.Hasher;
+import template.rand.RandomWrapper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class DLabellingCities {
+    public void solve(int testNumber, FastInput in, FastOutput out) {
+        int n = in.readInt();
+        int m = in.readInt();
+
+        Node[] nodes = new Node[n];
+        for (int i = 0; i < n; i++) {
+            nodes[i] = new Node();
+            nodes[i].id = i;
+            nodes[i].adj.add(nodes[i]);
+        }
+        for (int i = 0; i < m; i++) {
+            Node a = nodes[in.readInt() - 1];
+            Node b = nodes[in.readInt() - 1];
+            a.adj.add(b);
+            b.adj.add(a);
+        }
+
+        Map<Long, Node> map = new HashMap<>(n);
+
+        Modular mod = new Modular(1e9 + 7);
+        long x1 = RandomWrapper.INSTANCE.nextInt(3, mod.getMod() - 1);
+        long x2 = RandomWrapper.INSTANCE.nextInt(3, mod.getMod() - 1);
+
+        for (Node node : nodes) {
+            node.adj.sort((a, b) -> a.id - b.id);
+            int h1 = 1;
+            int h2 = 1;
+            for (Node x : node.adj) {
+                h1 = mod.valueOf(h1 * x1 + x.id);
+                h2 = mod.valueOf(h2 * x2 + x.id);
+            }
+            node.hash = DigitUtils.asLong(h1, h2);
+
+            Node set = map.get(node.hash);
+            if (set != null) {
+                node.set = set;
+            } else {
+                node.set = node;
+                map.put(node.hash, node);
+            }
+        }
+
+        boolean valid = true;
+        int[] cnts = new int[n + 1];
+        Node root = null;
+        for (Node node : nodes) {
+            if (node.set != node) {
+                continue;
+            }
+            for (Node x : node.adj) {
+                node.unique.add(x.set);
+            }
+            node.unique.remove(node.set);
+            if (node.unique.size() > 2) {
+                valid = false;
+            }
+            if (node.unique.size() <= 1) {
+                root = node;
+            }
+            cnts[node.unique.size()]++;
+        }
+
+        //circle
+        if (cnts[0] + cnts[1] == 0) {
+            valid = false;
+        }
+
+        if (!valid) {
+            out.println("NO");
+            return;
+        }
+
+        List<Node> list = new ArrayList<>(n);
+        while (root != null) {
+            if (list.size() > 0) {
+                root.unique.remove(list.get(list.size() - 1));
+            }
+            list.add(root);
+            Node next = null;
+            for (Node node : root.unique) {
+                next = node;
+            }
+            root = next;
+        }
+
+        int alloc = 1;
+        for(Node node : list){
+            node.alloc = alloc++;
+        }
+
+        out.println("YES");
+        for(Node node : nodes){
+            out.append(node.set.alloc).append(' ');
+        }
+    }
+}
+
+class Node {
+    List<Node> adj = new ArrayList<>();
+    Set<Node> unique = new HashSet<>();
+
+    int id;
+    long hash;
+    Node set;
+
+    int alloc;
+}
--- output/Main.java
+++ output/Main.java
@@ -1,12 +1,16 @@
 import java.io.OutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.stream.IntStream;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.util.Arrays;
 import java.io.IOException;
+import java.util.OptionalInt;
 import java.io.UncheckedIOException;
+import java.util.TreeMap;
+import java.util.Map;
 import java.io.Closeable;
+import java.util.Map.Entry;
 import java.io.Writer;
 import java.io.OutputStreamWriter;
 import java.io.InputStream;
@@ -29,116 +33,55 @@ public class Main {
             OutputStream outputStream = System.out;
             FastInput in = new FastInput(inputStream);
             FastOutput out = new FastOutput(outputStream);
-            FLehaAndSecuritySystem solver = new FLehaAndSecuritySystem();
+            EChoosingCarrot solver = new EChoosingCarrot();
             solver.solve(1, in, out);
             out.close();
         }
     }
 
-    static class FLehaAndSecuritySystem {
-        Debug debug = new Debug(true);
-
+    static class EChoosingCarrot {
         public void solve(int testNumber, FastInput in, FastOutput out) {
             int n = in.readInt();
-            int q = in.readInt();
-            int[] a = new int[n + 1];
-            for (int i = 1; i <= n; i++) {
-                a[i] = in.readInt();
-            }
+            int[] a = new int[n];
+            in.populate(a);
 
-            Segment seg = new Segment(1, n, i -> a[i]);
-            int[] cast = new int[10];
-            debug.debug("seg", seg);
-            for (int i = 0; i < q; i++) {
-                int t = in.readInt();
-                if (t == 1) {
-                    int l = in.readInt();
-                    int r = in.readInt();
-                    int x = in.readInt();
-                    int y = in.readInt();
-                    Segment.asStandard(cast);
-                    cast[x] = y;
-                    seg.update(l, r, 1, n, cast);
-                    debug.debug("seg", seg);
-                } else {
-                    int l = in.readInt();
-                    int r = in.readInt();
-                    long sum = seg.query(l, r, 1, n);
-                    out.println(sum);
+            int[] ans = new int[n];
+
+            //odd
+            TreeMap<Integer, Integer> odd = new TreeMap<>();
+            TreeMap<Integer, Integer> even = new TreeMap<>();
+            for (int i = 0; i < n; i++) {
+                if (i > 0 && i + 1 < n) {
+                    int len = Math.min(i - 1, n - 1 - (i + 1)) * 2 + 3;
+                    int val = Math.max(Math.min(a[i], a[i - 1]), Math.min(a[i], a[i + 1]));
+                    odd.put(val, Math.max(len, odd.getOrDefault(val, 0)));
+                }
+                if (i > 0) {
+                    int max = Math.max(a[i - 1], a[i]);
+                    int evenLen = Math.min(i - 1, n - 1 - i) * 2 + 2;
+                    even.put(max, Math.max(evenLen, even.getOrDefault(max, 0)));
                 }
             }
-        }
 
-    }
-
-    static interface IntToIntFunction {
-        int apply(int x);
-
-    }
-
-    static class FastOutput implements AutoCloseable, Closeable, Appendable {
-        private StringBuilder cache = new StringBuilder(10 << 20);
-        private final Writer os;
-
-        public FastOutput append(CharSequence csq) {
-            cache.append(csq);
-            return this;
-        }
-
-        public FastOutput append(CharSequence csq, int start, int end) {
-            cache.append(csq, start, end);
-            return this;
-        }
-
-        public FastOutput(Writer os) {
-            this.os = os;
-        }
-
-        public FastOutput(OutputStream os) {
-            this(new OutputStreamWriter(os));
-        }
-
-        public FastOutput append(char c) {
-            cache.append(c);
-            return this;
-        }
-
-        public FastOutput append(long c) {
-            cache.append(c);
-            return this;
-        }
-
-        public FastOutput println(long c) {
-            return append(c).println();
-        }
-
-        public FastOutput println() {
-            cache.append(System.lineSeparator());
-            return this;
-        }
-
-        public FastOutput flush() {
-            try {
-                os.append(cache);
-                os.flush();
-                cache.setLength(0);
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
+            ans[n - 1] = Arrays.stream(a).max().getAsInt();
+            for (int i = n - 2; i >= 0; i--) {
+                int remain = n - i;
+                if (remain % 2 == 1) {
+                    while (odd.lastEntry().getValue() < remain) {
+                        odd.pollLastEntry();
+                    }
+                    ans[i] = odd.lastKey();
+                } else {
+                    while (even.lastEntry().getValue() < remain) {
+                        even.pollLastEntry();
+                    }
+                    ans[i] = even.lastKey();
+                }
             }
-            return this;
-        }
 
-        public void close() {
-            flush();
-            try {
-                os.close();
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
+            for (int x : ans) {
+                out.append(x).append(' ');
             }
-        }
-
-        public String toString() {
-            return cache.toString();
         }
 
     }
@@ -152,6 +95,12 @@ public class Main {
 
         public FastInput(InputStream is) {
             this.is = is;
+        }
+
+        public void populate(int[] data) {
+            for (int i = 0; i < data.length; i++) {
+                data[i] = readInt();
+            }
         }
 
         private int read() {
@@ -202,231 +151,60 @@ public class Main {
 
     }
 
-    static class Debug {
-        private boolean offline;
-        private PrintStream out = System.err;
-        static int[] empty = new int[0];
+    static class FastOutput implements AutoCloseable, Closeable, Appendable {
+        private StringBuilder cache = new StringBuilder(10 << 20);
+        private final Writer os;
 
-        public Debug(boolean enable) {
-            offline = enable && System.getSecurityManager() == null;
+        public FastOutput append(CharSequence csq) {
+            cache.append(csq);
+            return this;
         }
 
-        public Debug debug(String name, Object x) {
-            return debug(name, x, empty);
+        public FastOutput append(CharSequence csq, int start, int end) {
+            cache.append(csq, start, end);
+            return this;
         }
 
-        public Debug debug(String name, Object x, int... indexes) {
-            if (offline) {
-                if (x == null || !x.getClass().isArray()) {
-                    out.append(name);
-                    for (int i : indexes) {
-                        out.printf("[%d]", i);
-                    }
-                    out.append("=").append("" + x);
-                    out.println();
-                } else {
-                    indexes = Arrays.copyOf(indexes, indexes.length + 1);
-                    if (x instanceof byte[]) {
-                        byte[] arr = (byte[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else if (x instanceof short[]) {
-                        short[] arr = (short[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else if (x instanceof boolean[]) {
-                        boolean[] arr = (boolean[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else if (x instanceof char[]) {
-                        char[] arr = (char[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else if (x instanceof int[]) {
-                        int[] arr = (int[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else if (x instanceof float[]) {
-                        float[] arr = (float[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else if (x instanceof double[]) {
-                        double[] arr = (double[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else if (x instanceof long[]) {
-                        long[] arr = (long[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else {
-                        Object[] arr = (Object[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    }
-                }
+        public FastOutput(Writer os) {
+            this.os = os;
+        }
+
+        public FastOutput(OutputStream os) {
+            this(new OutputStreamWriter(os));
+        }
+
+        public FastOutput append(char c) {
+            cache.append(c);
+            return this;
+        }
+
+        public FastOutput append(int c) {
+            cache.append(c);
+            return this;
+        }
+
+        public FastOutput flush() {
+            try {
+                os.append(cache);
+                os.flush();
+                cache.setLength(0);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
             }
             return this;
         }
 
-    }
-
-    static class Segment implements Cloneable {
-        private Segment left;
-        private Segment right;
-        static long[] buf = new long[10];
-        long[] sum = new long[10];
-        int[] dirty = new int[10];
-        boolean tag = false;
-
-        public static void asStandard(int[] x) {
-            for (int i = 0; i < 10; i++) {
-                x[i] = i;
-            }
-        }
-
-        private void modify(int[] x) {
-            Arrays.fill(buf, 0);
-            for (int i = 0; i < 10; i++) {
-                dirty[i] = x[dirty[i]];
-                buf[x[i]] += sum[i];
-            }
-            for (int i = 0; i < 10; i++) {
-                sum[i] = buf[i];
-            }
-            tag = true;
-        }
-
-        public void pushUp() {
-            for (int i = 0; i < 10; i++) {
-                sum[i] = left.sum[i] + right.sum[i];
-            }
-        }
-
-        public void pushDown() {
-            if (tag) {
-                left.modify(dirty);
-                right.modify(dirty);
-                tag = false;
-                asStandard(dirty);
-            }
-        }
-
-        public Segment(int l, int r, IntToIntFunction func) {
-            asStandard(dirty);
-            if (l < r) {
-                int m = (l + r) >> 1;
-                left = new Segment(l, m, func);
-                right = new Segment(m + 1, r, func);
-                pushUp();
-            } else {
-                int val = func.apply(l);
-                int base = 1;
-                while (val != 0) {
-                    sum[val % 10] += base;
-                    base *= 10;
-                    val /= 10;
-                }
-            }
-        }
-
-        private boolean covered(int ll, int rr, int l, int r) {
-            return ll <= l && rr >= r;
-        }
-
-        private boolean noIntersection(int ll, int rr, int l, int r) {
-            return ll > r || rr < l;
-        }
-
-        public void update(int ll, int rr, int l, int r, int[] x) {
-            if (noIntersection(ll, rr, l, r)) {
-                return;
-            }
-            if (covered(ll, rr, l, r)) {
-                modify(x);
-                return;
-            }
-            pushDown();
-            int m = (l + r) >> 1;
-            left.update(ll, rr, l, m, x);
-            right.update(ll, rr, m + 1, r, x);
-            pushUp();
-        }
-
-        private static long parse(long[] sum) {
-            long ans = 0;
-            for (int i = 0; i < 10; i++) {
-                ans += sum[i] * i;
-            }
-            return ans;
-        }
-
-        public long query(int ll, int rr, int l, int r) {
-            if (noIntersection(ll, rr, l, r)) {
-                return 0;
-            }
-            if (covered(ll, rr, l, r)) {
-                return parse(sum);
-            }
-            pushDown();
-            int m = (l + r) >> 1;
-            return left.query(ll, rr, l, m) +
-                    right.query(ll, rr, m + 1, r);
-        }
-
-        private Segment deepClone() {
-            Segment seg = clone();
-            if (seg.left != null) {
-                seg.left = seg.left.deepClone();
-            }
-            if (seg.right != null) {
-                seg.right = seg.right.deepClone();
-            }
-            return seg;
-        }
-
-        protected Segment clone() {
+        public void close() {
+            flush();
             try {
-                return (Segment) super.clone();
-            } catch (CloneNotSupportedException e) {
-                throw new RuntimeException(e);
+                os.close();
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
             }
-        }
-
-        private void toString(StringBuilder builder) {
-            if (left == null && right == null) {
-                builder.append(parse(sum)).append(",");
-                return;
-            }
-            pushDown();
-            left.toString(builder);
-            right.toString(builder);
         }
 
         public String toString() {
-            StringBuilder builder = new StringBuilder();
-            deepClone().toString(builder);
-            if (builder.length() > 0) {
-                builder.setLength(builder.length() - 1);
-            }
-            return builder.toString();
+            return cache.toString();
         }
 
     }
