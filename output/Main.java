fcb73815643960c769856e75f81057e50fde1785
taodaling <taodaling@gmail.com>
2020-06-03 01:05:05 +0800

    solve more

--- output/Main.java
+++ output/Main.java
@@ -2,6 +2,7 @@ import java.io.OutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.io.Closeable;
@@ -27,32 +28,306 @@ public class Main {
             OutputStream outputStream = System.out;
             FastInput in = new FastInput(inputStream);
             FastOutput out = new FastOutput(outputStream);
-            AVasyaAndTriangle solver = new AVasyaAndTriangle();
+            CPuttingBoxesTogether solver = new CPuttingBoxesTogether();
             solver.solve(1, in, out);
             out.close();
         }
     }
 
-    static class AVasyaAndTriangle {
+    static class CPuttingBoxesTogether {
+        static Modular mod = new Modular(1e9 + 7);
+        Debug debug = new Debug(true);
+
         public void solve(int testNumber, FastInput in, FastOutput out) {
-            long n = in.readInt();
-            long m = in.readInt();
-            long k = in.readInt();
-            if (2 * n * m % k != 0) {
-                out.println("NO");
+            int n = in.readInt();
+            int q = in.readInt();
+            int[] a = new int[n + 1];
+            int[] w = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                a[i] = in.readInt();
+            }
+            for (int i = 1; i <= n; i++) {
+                w[i] = in.readInt();
+            }
+
+            Query query = new Query();
+            State state = new State();
+            Segment seg = new Segment(1, n, i -> w[i], i -> a[i]);
+            for (int i = 0; i < q; i++) {
+                int x = in.readInt();
+                int y = in.readInt();
+                if (x < 0) {
+                    seg.update(-x, -x, 1, n, y);
+                } else {
+                    int l = x;
+                    int r = y;
+                    state.clear(0);
+                    seg.query(l, r, 1, n, state);
+                    long sumOfW = state.w;
+                    query.reset(0, sumOfW);
+                    seg.binarySearch(l, r, 1, n, query);
+                    int index = query.index;
+                    debug.debug("index", index);
+                    long ans = 0;
+                    state.clear(a[index]);
+                    seg.query(l, index, 1, n, state);
+                    ans += state.moveR;
+                    state.clear(a[index]);
+                    seg.query(index, r, 1, n, state);
+                    ans += state.moveL;
+                    out.println(mod.valueOf(ans));
+                }
+            }
+        }
+
+    }
+
+    static class Modular {
+        int m;
+
+        public Modular(int m) {
+            this.m = m;
+        }
+
+        public Modular(long m) {
+            this.m = (int) m;
+            if (this.m != m) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        public Modular(double m) {
+            this.m = (int) m;
+            if (this.m != m) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        public int valueOf(int x) {
+            x %= m;
+            if (x < 0) {
+                x += m;
+            }
+            return x;
+        }
+
+        public int valueOf(long x) {
+            x %= m;
+            if (x < 0) {
+                x += m;
+            }
+            return (int) x;
+        }
+
+        public int mul(int x, int y) {
+            return valueOf((long) x * y);
+        }
+
+        public int plus(int x, int y) {
+            return valueOf(x + y);
+        }
+
+        public String toString() {
+            return "mod " + m;
+        }
+
+    }
+
+    static class Segment implements Cloneable {
+        private Segment left;
+        private Segment right;
+        State state = new State();
+
+        private void modify(int w) {
+            state.init(w);
+        }
+
+        public void pushUp() {
+            state.merge(left.state, right.state);
+        }
+
+        public void pushDown() {
+        }
+
+        public Segment(int l, int r, IntToIntFunction func, IntToIntFunction index) {
+            if (l < r) {
+                int m = (l + r) >> 1;
+                left = new Segment(l, m, func, index);
+                right = new Segment(m + 1, r, func, index);
+                pushUp();
+            } else {
+                modify(func.apply(l));
+                state.l = state.r = index.apply(l);
+                state.size = 1;
+            }
+        }
+
+        private boolean covered(int ll, int rr, int l, int r) {
+            return ll <= l && rr >= r;
+        }
+
+        private boolean noIntersection(int ll, int rr, int l, int r) {
+            return ll > r || rr < l;
+        }
+
+        public void update(int ll, int rr, int l, int r, int x) {
+            if (noIntersection(ll, rr, l, r)) {
                 return;
             }
-            out.println("YES");
-            out.println("0 0");
-            long area = 2 * n * m / k;
+            if (covered(ll, rr, l, r)) {
+                modify(x);
+                return;
+            }
+            pushDown();
+            int m = (l + r) >> 1;
+            left.update(ll, rr, l, m, x);
+            right.update(ll, rr, m + 1, r, x);
+            pushUp();
+        }
 
-            long x3 = 1;
-            long y3 = m;
-            long x2 = DigitUtils.ceilDiv(area, y3);
-            long y2 = x2 * y3 - area;
+        public void query(int ll, int rr, int l, int r, State ans) {
+            if (noIntersection(ll, rr, l, r)) {
+                return;
+            }
+            if (covered(ll, rr, l, r)) {
+                ans.mergeRight(state);
+                return;
+            }
+            pushDown();
+            int m = (l + r) >> 1;
+            left.query(ll, rr, l, m, ans);
+            right.query(ll, rr, m + 1, r, ans);
+        }
 
-            out.append(x2).append(' ').append(y2).println();
-            out.append(x3).append(' ').append(y3).println();
+        public void binarySearch(int ll, int rr, int l, int r, Query q) {
+            if (noIntersection(ll, rr, l, r) || q.now * 2 >= q.total) {
+                return;
+            }
+            if (covered(ll, rr, l, r) && (q.now + state.w) * 2 < q.total) {
+                q.now += state.w;
+                return;
+            }
+            if (l == r) {
+                q.now += state.w;
+                q.index = l;
+                return;
+            }
+            pushDown();
+            int m = (l + r) >> 1;
+            left.binarySearch(ll, rr, l, m, q);
+            right.binarySearch(ll, rr, m + 1, r, q);
+        }
+
+        private Segment deepClone() {
+            Segment seg = clone();
+            if (seg.left != null) {
+                seg.left = seg.left.deepClone();
+            }
+            if (seg.right != null) {
+                seg.right = seg.right.deepClone();
+            }
+            return seg;
+        }
+
+        protected Segment clone() {
+            try {
+                return (Segment) super.clone();
+            } catch (CloneNotSupportedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        private void toString(StringBuilder builder) {
+            if (left == null && right == null) {
+                builder.append(state.w).append(",");
+                return;
+            }
+            pushDown();
+            left.toString(builder);
+            right.toString(builder);
+        }
+
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            deepClone().toString(builder);
+            if (builder.length() > 0) {
+                builder.setLength(builder.length() - 1);
+            }
+            return builder.toString();
+        }
+
+    }
+
+    static class Debug {
+        private boolean offline;
+        private PrintStream out = System.err;
+
+        public Debug(boolean enable) {
+            offline = enable && System.getSecurityManager() == null;
+        }
+
+        public Debug debug(String name, int x) {
+            if (offline) {
+                debug(name, "" + x);
+            }
+            return this;
+        }
+
+        public Debug debug(String name, String x) {
+            if (offline) {
+                out.printf("%s=%s", name, x);
+                out.println();
+            }
+            return this;
+        }
+
+    }
+
+    static class State {
+        static Modular mod = new Modular(1e9 + 7);
+        int l;
+        int r;
+        long w;
+        int moveL;
+        int moveR;
+        int size;
+        static State tmp = new State();
+
+        public void clear(int i) {
+            l = r = i;
+            w = 0;
+            moveL = moveR = 0;
+            size = 0;
+        }
+
+        public void init(int w) {
+            this.w = w;
+            moveL = moveR = 0;
+        }
+
+        public void copy(State s) {
+            l = s.l;
+            r = s.r;
+            w = s.w;
+            moveL = s.moveL;
+            moveR = s.moveR;
+            size = s.size;
+        }
+
+        public void mergeRight(State r) {
+            tmp.copy(this);
+            merge(tmp, r);
+        }
+
+        public void merge(State a, State b) {
+            l = a.l;
+            r = b.r;
+            w = a.w + b.w;
+            size = a.size + b.size;
+            moveL = mod.plus(a.moveL, b.moveL);
+            moveL = mod.plus(moveL, mod.mul(mod.valueOf(b.w), b.l - a.l - a.size));
+            moveR = mod.plus(a.moveR, b.moveR);
+            moveR = mod.plus(moveR, mod.mul(mod.valueOf(a.w), b.r - a.r - b.size));
         }
 
     }
@@ -116,24 +391,21 @@ public class Main {
 
     }
 
-    static class DigitUtils {
-        private DigitUtils() {
+    static class Query {
+        long now;
+        long total;
+        int index;
+
+        public void reset(long now, long total) {
+            this.now = now;
+            this.total = total;
+            index = -1;
         }
 
-        public static long floorDiv(long a, long b) {
-            return a < 0 ? -ceilDiv(-a, b) : a / b;
-        }
+    }
 
-        public static long ceilDiv(long a, long b) {
-            if (a < 0) {
-                return -floorDiv(-a, b);
-            }
-            long c = a / b;
-            if (c * b < a) {
-                return c + 1;
-            }
-            return c;
-        }
+    static interface IntToIntFunction {
+        int apply(int x);
 
     }
 
@@ -164,17 +436,12 @@ public class Main {
             return this;
         }
 
-        public FastOutput append(long c) {
+        public FastOutput append(int c) {
             cache.append(c);
             return this;
         }
 
-        public FastOutput append(String c) {
-            cache.append(c);
-            return this;
-        }
-
-        public FastOutput println(String c) {
+        public FastOutput println(int c) {
             return append(c).println();
         }
 
---
+++ src/test/java/on2020_06/on2020_06_02_Codeforces___Codeforces_Round__512__Div__1__based_on_Technocup_2019_Elimination_Round_1_/C__Putting_Boxes_Together/Main.java
@@ -0,0 +1,14 @@
+package on2020_06.on2020_06_02_Codeforces___Codeforces_Round__512__Div__1__based_on_Technocup_2019_Elimination_Round_1_.C__Putting_Boxes_Together;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("src/test/java/on2020_06/on2020_06_02_Codeforces___Codeforces_Round__512__Div__1__based_on_Technocup_2019_Elimination_Round_1_/C__Putting_Boxes_Together/C. Putting Boxes Together.json"))
+			Assert.fail();
+	}
+}
