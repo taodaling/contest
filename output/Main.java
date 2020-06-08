139ca11aff2e19355cc57b0728d15e73ea584934
taodaling <daling.tao@ideacome.com>
2020-06-08 16:05:01 +0800

    每日一题

--- output/Main.java
+++ output/Main.java
@@ -1,14 +1,20 @@
 import java.io.OutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.PriorityQueue;
+import java.util.AbstractQueue;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.AbstractCollection;
+import java.io.OutputStreamWriter;
 import java.io.OutputStream;
-import java.io.PrintStream;
-import java.util.Arrays;
+import java.util.Iterator;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.List;
 import java.io.Closeable;
 import java.io.Writer;
-import java.io.OutputStreamWriter;
+import java.util.Comparator;
 import java.io.InputStream;
 
 /**
@@ -29,171 +35,230 @@ public class Main {
             OutputStream outputStream = System.out;
             FastInput in = new FastInput(inputStream);
             FastOutput out = new FastOutput(outputStream);
-            CRangeSet solver = new CRangeSet();
-            solver.solve(1, in, out);
+            EJamesAndTheChase solver = new EJamesAndTheChase();
+            int testCount = Integer.parseInt(in.next());
+            for (int i = 1; i <= testCount; i++)
+                solver.solve(i, in, out);
             out.close();
         }
     }
 
-    static class CRangeSet {
-        Debug debug = new Debug(false);
-        Modular mod = new Modular(1e9 + 7);
-        Power pow = new Power(mod);
-        int a;
-        int b;
-        int[] f;
+    static class EJamesAndTheChase {
+        RandomWrapper rw = new RandomWrapper();
+        boolean valid;
 
         public void solve(int testNumber, FastInput in, FastOutput out) {
             int n = in.readInt();
-            a = in.readInt();
-            b = in.readInt();
-            if (a < b) {
-                int tmp = a;
-                a = b;
-                b = tmp;
+            int m = in.readInt();
+
+            Node[] nodes = new Node[n];
+            for (int i = 0; i < n; i++) {
+                nodes[i] = new Node();
+                nodes[i].id = i;
             }
 
-            f = new int[a];
-            SequenceUtils.deepFill(f, -1);
+            for (int i = 0; i < m; i++) {
+                Node a = nodes[in.readInt() - 1];
+                Node b = nodes[in.readInt() - 1];
+                a.adj.add(b);
+            }
 
-//        for (int i = 0; i < a; i++) {
-//            debug.debug("f(" + i + ")", f(i));
-//        }
+            Node root = null;
 
-            ArrayIndex ai = new ArrayIndex(n + 1, a, 2);
-            int[] dp = new int[ai.totalSize()];
-            dp[ai.indexOf(0, 0, 0)] = 1;
-            dp[ai.indexOf(0, 0, 1)] = 1;
-            int[] sum = new int[2];
-            for (int i = 1; i <= n; i++) {
-                Arrays.fill(sum, 0);
-                for (int j = 0; j < a; j++) {
-                    int lend = j == i - 1 ? 1 : 0;
-                    sum[0] = mod.plus(sum[0], mod.mul(dp[ai.indexOf(i - 1, j, 0)], f(j - 2 + lend)));
+            for (int i = 0; i < 50; i++) {
+                Node randRoot = nodes[rw.nextInt(0, n - 1)];
+                valid = true;
+                for (Node node : nodes) {
+                    node.visited = false;
+                    node.instk = false;
                 }
-                for (int j = 0; j < b; j++) {
-                    sum[1] = mod.plus(sum[1], dp[ai.indexOf(i - 1, j, 1)]);
+
+                dfs(randRoot, 0);
+                for (Node node : nodes) {
+                    valid = valid && node.visited;
                 }
-                debug.debug("i", i - 1);
-                debug.debug("sum", sum);
-                for (int k = 1; k < a; k++) {
-                    if (k == 1) {
-                        dp[ai.indexOf(i, k, 0)] = sum[1];
-                    } else {
-                        dp[ai.indexOf(i, k, 0)] = dp[ai.indexOf(i - 1, k - 1, 0)];
-                    }
-                }
-                for (int k = 1; k < b; k++) {
-                    if (k == 1) {
-                        dp[ai.indexOf(i, k, 1)] = sum[0];
-                    } else {
-                        dp[ai.indexOf(i, k, 1)] = dp[ai.indexOf(i - 1, k - 1, 1)];
-                    }
+
+                if (!valid) {
+                    continue;
+                } else {
+                    root = randRoot;
+                    break;
                 }
             }
 
-            Arrays.fill(sum, 0);
-            for (int j = 0; j < a; j++) {
-                sum[0] = mod.plus(sum[0], mod.mul(dp[ai.indexOf(n, j, 0)], f(j - 1)));
+            if (root == null) {
+                out.println(-1);
+                return;
             }
-            for (int j = 0; j < b; j++) {
-                sum[1] = mod.plus(sum[1], dp[ai.indexOf(n, j, 1)]);
-            }
-            debug.debug("sum", sum);
 
-            int invalid = mod.plus(sum[0], sum[1]);
-            int total = pow.pow(2, n);
-            int valid = mod.subtract(total, invalid);
-            out.println(valid);
+            //dfs and find root
+            dpOnTree(root);
+            root.ok = true;
+            countOnTree(root);
+
+            List<Node> interestingNodes = new ArrayList<>(n);
+            for (Node node : nodes) {
+                if (node.ok) {
+                    interestingNodes.add(node);
+                }
+            }
+
+            if (interestingNodes.size() * 5 < n) {
+                out.println(-1);
+                return;
+            }
+
+            //out.println(interestingNodes.size());
+            for (Node node : interestingNodes) {
+                out.append(node.id + 1).append(' ');
+            }
+            out.println();
         }
 
-        public int f(int i) {
-            if (i <= 0) {
-                return 1;
+        public void countOnTree(Node root) {
+            if (root.top != null && root.top.ok) {
+                root.ok = true;
             }
-            if (f[i] == -1) {
-                f[i] = f(i - 1);
-                for (int t = b; i - t >= 0; t++) {
-                    f[i] = mod.plus(f[i], f(i - t - 1));
+            for (Node node : root.adj) {
+                if (node.depth != root.depth + 1) {
+                    continue;
+                }
+                countOnTree(node);
+            }
+        }
+
+        public void dpOnTree(Node root) {
+            for (Node node : root.adj) {
+                if (node.depth != root.depth + 1) {
+                    //to ancestor
+                    root.pq.add(node);
+                } else {
+                    dpOnTree(node);
+                    root.pq.addAll(node.pq);
                 }
             }
-            return f[i];
+
+            while (!root.pq.isEmpty() && root.pq.peekMax().depth >= root.depth) {
+                root.pq.popMax();
+            }
+
+            if (root.pq.size() == 1) {
+                root.top = root.pq.iterator().next();
+            }
+        }
+
+        public void dfs(Node root, int depth) {
+            if (root.visited) {
+                valid = valid && root.instk;
+                return;
+            }
+
+            root.visited = true;
+            root.instk = true;
+            root.depth = depth;
+
+            for (Node node : root.adj) {
+                dfs(node, depth + 1);
+            }
+
+            root.instk = false;
         }
 
     }
 
-    static class Power implements InverseNumber {
-        final Modular modular;
+    static class FastInput {
+        private final InputStream is;
+        private StringBuilder defaultStringBuf = new StringBuilder(1 << 13);
+        private byte[] buf = new byte[1 << 13];
+        private int bufLen;
+        private int bufOffset;
+        private int next;
 
-        public Power(Modular modular) {
-            this.modular = modular;
+        public FastInput(InputStream is) {
+            this.is = is;
         }
 
-        public int pow(int x, int n) {
-            if (n == 0) {
-                return modular.valueOf(1);
+        private int read() {
+            while (bufLen == bufOffset) {
+                bufOffset = 0;
+                try {
+                    bufLen = is.read(buf);
+                } catch (IOException e) {
+                    bufLen = -1;
+                }
+                if (bufLen == -1) {
+                    return -1;
+                }
             }
-            long r = pow(x, n >> 1);
-            r = modular.valueOf(r * r);
-            if ((n & 1) == 1) {
-                r = modular.valueOf(r * x);
+            return buf[bufOffset++];
+        }
+
+        public void skipBlank() {
+            while (next >= 0 && next <= 32) {
+                next = read();
             }
-            return (int) r;
+        }
+
+        public String next() {
+            return readString();
+        }
+
+        public int readInt() {
+            int sign = 1;
+
+            skipBlank();
+            if (next == '+' || next == '-') {
+                sign = next == '+' ? 1 : -1;
+                next = read();
+            }
+
+            int val = 0;
+            if (sign == 1) {
+                while (next >= '0' && next <= '9') {
+                    val = val * 10 + next - '0';
+                    next = read();
+                }
+            } else {
+                while (next >= '0' && next <= '9') {
+                    val = val * 10 - next + '0';
+                    next = read();
+                }
+            }
+
+            return val;
+        }
+
+        public String readString(StringBuilder builder) {
+            skipBlank();
+
+            while (next > 32) {
+                builder.append((char) next);
+                next = read();
+            }
+
+            return builder.toString();
+        }
+
+        public String readString() {
+            defaultStringBuf.setLength(0);
+            return readString(defaultStringBuf);
         }
 
     }
 
-    static class Modular {
-        int m;
+    static class RandomWrapper {
+        private Random random;
 
-        public Modular(int m) {
-            this.m = m;
+        public RandomWrapper() {
+            this(new Random());
         }
 
-        public Modular(long m) {
-            this.m = (int) m;
-            if (this.m != m) {
-                throw new IllegalArgumentException();
-            }
+        public RandomWrapper(Random random) {
+            this.random = random;
         }
 
-        public Modular(double m) {
-            this.m = (int) m;
-            if (this.m != m) {
-                throw new IllegalArgumentException();
-            }
-        }
-
-        public int valueOf(int x) {
-            x %= m;
-            if (x < 0) {
-                x += m;
-            }
-            return x;
-        }
-
-        public int valueOf(long x) {
-            x %= m;
-            if (x < 0) {
-                x += m;
-            }
-            return (int) x;
-        }
-
-        public int mul(int x, int y) {
-            return valueOf((long) x * y);
-        }
-
-        public int plus(int x, int y) {
-            return valueOf(x + y);
-        }
-
-        public int subtract(int x, int y) {
-            return valueOf(x - y);
-        }
-
-        public String toString() {
-            return "mod " + m;
+        public int nextInt(int l, int r) {
+            return random.nextInt(r - l + 1) + l;
         }
 
     }
@@ -265,208 +330,68 @@ public class Main {
 
     }
 
-    static class Debug {
-        private boolean offline;
-        private PrintStream out = System.err;
-        static int[] empty = new int[0];
+    static class FixedMinCollection<V> extends AbstractCollection<V> {
+        private PriorityQueue<V> pq;
+        private int cap;
+        private Comparator<V> comp;
 
-        public Debug(boolean enable) {
-            offline = enable && System.getSecurityManager() == null;
-        }
-
-        public Debug debug(String name, int x) {
-            if (offline) {
-                debug(name, "" + x);
-            }
-            return this;
-        }
-
-        public Debug debug(String name, String x) {
-            if (offline) {
-                out.printf("%s=%s", name, x);
-                out.println();
-            }
-            return this;
-        }
-
-        public Debug debug(String name, Object x) {
-            return debug(name, x, empty);
-        }
-
-        public Debug debug(String name, Object x, int... indexes) {
-            if (offline) {
-                if (x == null || !x.getClass().isArray()) {
-                    out.append(name);
-                    for (int i : indexes) {
-                        out.printf("[%d]", i);
-                    }
-                    out.append("=").append("" + x);
-                    out.println();
-                } else {
-                    indexes = Arrays.copyOf(indexes, indexes.length + 1);
-                    if (x instanceof byte[]) {
-                        byte[] arr = (byte[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else if (x instanceof short[]) {
-                        short[] arr = (short[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else if (x instanceof boolean[]) {
-                        boolean[] arr = (boolean[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else if (x instanceof char[]) {
-                        char[] arr = (char[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else if (x instanceof int[]) {
-                        int[] arr = (int[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else if (x instanceof float[]) {
-                        float[] arr = (float[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else if (x instanceof double[]) {
-                        double[] arr = (double[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else if (x instanceof long[]) {
-                        long[] arr = (long[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    } else {
-                        Object[] arr = (Object[]) x;
-                        for (int i = 0; i < arr.length; i++) {
-                            indexes[indexes.length - 1] = i;
-                            debug(name, arr[i], indexes);
-                        }
-                    }
-                }
-            }
-            return this;
-        }
-
-    }
-
-    static class FastInput {
-        private final InputStream is;
-        private byte[] buf = new byte[1 << 13];
-        private int bufLen;
-        private int bufOffset;
-        private int next;
-
-        public FastInput(InputStream is) {
-            this.is = is;
-        }
-
-        private int read() {
-            while (bufLen == bufOffset) {
-                bufOffset = 0;
-                try {
-                    bufLen = is.read(buf);
-                } catch (IOException e) {
-                    bufLen = -1;
-                }
-                if (bufLen == -1) {
-                    return -1;
-                }
-            }
-            return buf[bufOffset++];
-        }
-
-        public void skipBlank() {
-            while (next >= 0 && next <= 32) {
-                next = read();
-            }
-        }
-
-        public int readInt() {
-            int sign = 1;
-
-            skipBlank();
-            if (next == '+' || next == '-') {
-                sign = next == '+' ? 1 : -1;
-                next = read();
-            }
-
-            int val = 0;
-            if (sign == 1) {
-                while (next >= '0' && next <= '9') {
-                    val = val * 10 + next - '0';
-                    next = read();
-                }
-            } else {
-                while (next >= '0' && next <= '9') {
-                    val = val * 10 - next + '0';
-                    next = read();
-                }
-            }
-
-            return val;
-        }
-
-    }
-
-    static class SequenceUtils {
-        public static void deepFill(Object array, int val) {
-            if (!array.getClass().isArray()) {
+        public FixedMinCollection(int cap, Comparator<V> comp) {
+            if (cap == 0) {
                 throw new IllegalArgumentException();
             }
-            if (array instanceof int[]) {
-                int[] intArray = (int[]) array;
-                Arrays.fill(intArray, val);
-            } else {
-                Object[] objArray = (Object[]) array;
-                for (Object obj : objArray) {
-                    deepFill(obj, val);
-                }
+            this.cap = cap;
+            this.comp = comp;
+            pq = new PriorityQueue<>(cap, comp.reversed());
+        }
+
+        public boolean add(V v) {
+            if (pq.size() < cap) {
+                pq.add(v);
+                return true;
             }
+            if (comp.compare(pq.peek(), v) > 0) {
+                pq.remove();
+                pq.add(v);
+                return true;
+            }
+            return false;
+        }
+
+        public V peekMax() {
+            return pq.peek();
+        }
+
+        public V popMax() {
+            return pq.remove();
+        }
+
+        public void clear() {
+            pq.clear();
+        }
+
+        public Iterator<V> iterator() {
+            return pq.iterator();
+        }
+
+        public int size() {
+            return pq.size();
         }
 
     }
 
-    static interface InverseNumber {
-    }
+    static class Node {
+        List<Node> adj = new ArrayList<>();
+        int id;
+        boolean visited;
+        boolean instk;
+        int depth;
+        Node top;
+        boolean ok;
+        static Comparator<Node> sortByDepth = (a, b) -> Integer.compare(a.depth, b.depth);
+        FixedMinCollection<Node> pq = new FixedMinCollection<>(2, sortByDepth);
 
-    static class ArrayIndex {
-        int[] dimensions;
-
-        public ArrayIndex(int... dimensions) {
-            this.dimensions = dimensions;
-        }
-
-        public int totalSize() {
-            int ans = 1;
-            for (int x : dimensions) {
-                ans *= x;
-            }
-            return ans;
-        }
-
-        public int indexOf(int a, int b) {
-            return a * dimensions[1] + b;
-        }
-
-        public int indexOf(int a, int b, int c) {
-            return indexOf(a, b) * dimensions[2] + c;
+        public String toString() {
+            return "" + (id + 1);
         }
 
     }
---
+++ src/test/java/on2020_06/on2020_06_08_Codeforces___Codeforces_Round__647__Div__1____Thanks__Algo_Muse_/E__James_and_the_Chase/Main.java
@@ -0,0 +1,14 @@
+package on2020_06.on2020_06_08_Codeforces___Codeforces_Round__647__Div__1____Thanks__Algo_Muse_.E__James_and_the_Chase;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("src/test/java/on2020_06/on2020_06_08_Codeforces___Codeforces_Round__647__Div__1____Thanks__Algo_Muse_/E__James_and_the_Chase/E. James and the Chase.json"))
+			Assert.fail();
+	}
+}
